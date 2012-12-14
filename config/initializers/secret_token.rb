0c54ddb8d81dca28b5ea6bd755f59564f2b8ab08
Adam Jenkins <adamcole83@gmail.com>
2012-12-14 17:16:27 -0600

    Moved application to Postgresql

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ElabraStats::Application.config.secret_token = 'fb11b1f829e3565de29bc9fce46e08d559a01d0b31d986460cfdc04c926543db82df0f9dad1734288457838515c9d612a3c5245716c00cfad112549461664d26'
+ElabraStats::Application.config.secret_token = '7b59d35337b1ba85324c42d1f36138c6cebf05e0925ac832bb5f5da05a24d0dee80b5d46b3c2fb3675397058e0ceee87671cb89315d548ced4c5851bc8970a49'
