d22ecc79caad97f3c33e6617adbd826af06973c9
Adam Jenkins <adamcole83@gmail.com>
2012-12-14 16:17:27 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElabraStats::Application.config.secret_token = 'fb11b1f829e3565de29bc9fce46e08d559a01d0b31d986460cfdc04c926543db82df0f9dad1734288457838515c9d612a3c5245716c00cfad112549461664d26'
